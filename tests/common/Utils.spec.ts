fe83512393a17966a1f0f0199f52eb81530251d7
Josh Hendershot <hendershotjoshr@gmail.com>
2021-02-05 14:59:12 -0500

    added stringSimilarity function

--- tests/common/Utils.spec.ts
+++ tests/common/Utils.spec.ts
@@ -11,3 +11,18 @@ describe("test isMissionaryEmail()", () => {
     expect(M_Utils.isMissionaryEmail("j@gmail.com")).toBe(false);
   });
 });
+
+describe("test stringSimilarity()", () => {
+  it("given 'test' and 'test' should return 1", () => {
+    expect(M_Utils.stringSimilarity("test", "test")).toBe(1);
+  });
+  it("given 'test' and 'no' should return 0", () => {
+    expect(M_Utils.stringSimilarity("test", "no")).toBe(0);
+  });
+  it("given 'test' and 'tset' should return 0.5", () => {
+    expect(M_Utils.stringSimilarity("test", "tset")).toBe(0.5);
+  });
+  it("given 'traverse city' and 'travesrs city' should return close to 0.8461538461538461", () => {
+    expect(M_Utils.stringSimilarity("traverse city", "travesrs city")).toBeCloseTo(0.8461538461538461);
+  });
+});
