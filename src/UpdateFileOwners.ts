3aa9881c7162195c2f0bccaae9f71ed31d970cb2
Josh Hendershot <hendershotjoshr@gmail.com>
2021-03-03 13:25:46 -0500

    added functionality

--- src/UpdateFileOwners.ts
+++ src/UpdateFileOwners.ts
@@ -1,17 +1,48 @@
-import { FlushContent as FlushContent } from "./FlushContent";
-import { Permissions } from "./Permissions";
-import { PDFSender } from "./PDFSender";
 import { Variables } from "./common/Variables";
+import { M_Utils } from "./common/Utils";
+import { SheetLogger } from "./common/SheetLogger";
 
 export { UpdateFileOwners };
 
 namespace UpdateFileOwners {
-  const flushContent = FlushContent;
-  const Perms = Permissions;
-  const pdfSender = PDFSender;
   const Vars = Variables;
+  const Utils = M_Utils;
+  const sheetLogger = SheetLogger.SheetLogger;
 
   export function updateOwners(): void {
-      
+    const folderSuffixes = [
+      Vars.getQuickFolderName(),
+      Vars.getQualityFolderName(),
+    ];
+
+    Utils.forEveryContentFolder(
+      DriveApp.getFolderById(Vars.getZoneDrivesID()),
+      (folder) => {
+        forEachFile(folder, (file) => changeOwnershipToAreaEmail(file, folder));
+      },
+      folderSuffixes
+    );
+  }
+
+  function forEachFile(
+    folder: GoogleAppsScript.Drive.Folder,
+    func: (file: GoogleAppsScript.Drive.File) => any
+  ): void {
+    const fileIterator = folder.getFiles();
+    while (fileIterator.hasNext()) {
+      const file = fileIterator.next();
+      func(file);
+    }
+  }
+
+  function changeOwnershipToAreaEmail(
+    file: GoogleAppsScript.Drive.File,
+    parentFolder: GoogleAppsScript.Drive.Folder
+  ) {
+    const areaEmail = parentFolder.getParents().next().getOwner();
+    sheetLogger.Log(
+      `setting owner of ${file.getName()} to area email: ${areaEmail.getEmail()}`
+    );
+    file.setOwner(areaEmail);
   }
 }
