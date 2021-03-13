9478923b219324caa6810cc77ed7c8fa8f4105e4
Josh Hendershot <hendershotjoshr@gmail.com>
2021-03-13 23:14:30 +0000

    simplified getNamedRange

--- src/common/Utils.ts
+++ src/common/Utils.ts
@@ -85,6 +85,7 @@ namespace M_Utils {
     }
 
     export function getNamedRange(spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet, name: string): GoogleAppsScript.Spreadsheet.Range {
+        return spreadsheet.getRangeByName(name);
         const ranges = spreadsheet.getNamedRanges();
         const filtered = ranges.filter((r) => r.getName() == name);
         if (filtered.length <= 0) {
